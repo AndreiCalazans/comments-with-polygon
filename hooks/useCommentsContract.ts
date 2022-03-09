cff925a1d3f17cbbe1a2d914c4309d586ddcf705
Andrei Calazans <andreixoc@hotmail.com>
2022-03-08 23:44:36 -0300

    Point to smart contract deployed to Mumbai test net

--- hooks/useCommentsContract.ts
+++ hooks/useCommentsContract.ts
@@ -30,7 +30,7 @@ const useCommentsContract = () => {
   // We also pass in the signer if there is a signed in wallet, or if there's
   // no signed in wallet then we'll pass in the connected provider.
   const contract = wagmi.useContract({
-    addressOrName: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    addressOrName: "0x15ea87DF7497314aCa76A62e403c0d5fE7bD5696",
     contractInterface: CommentsContract.abi,
     signerOrProvider: signer.data || provider,
   });
